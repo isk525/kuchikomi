12e59d73ba3cbbc7b8091acf6c89f5cb0a8b9f10
isk525 <y.isk.1108@gmail.com>
2026-09-05 22:48:36 +0900

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,26 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getAuth,signInAnonymously } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDdI9B-vZUNlBEpkrujxLaxDNWmgAdNouM",
-  authDomain: "kuchikomi-web.firebaseapp.com",
-  projectId: "kuchikomi-web",
-  storageBucket: "kuchikomi-web.firebasestorage.app",
-  messagingSenderId: "70374620856",
-  appId: "1:70374620856:web:05c78cf05f33415497be4d"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export function ensureAnonymousAuth() {
-  return new Promise((resolve, reject) => {
-    const stop = onAuthStateChanged(auth, async user => {
-      if (user) { stop(); resolve(user); return; }
-      try { await signInAnonymously(auth); }
-      catch (error) { stop(); reject(error); }
-    });
-  });
-}
+const firebaseConfig={apiKey:"AIzaSyDdI9B-vZUNlBEpkrujxLaxDNWmgAdNouM",authDomain:"kuchikomi-web.firebaseapp.com",projectId:"kuchikomi-web",storageBucket:"kuchikomi-web.firebasestorage.app",messagingSenderId:"70374620856",appId:"1:70374620856:web:05c78cf05f33415497be4d"};
+const app=initializeApp(firebaseConfig);export const auth=getAuth(app);export const db=getFirestore(app);signInAnonymously(auth);
